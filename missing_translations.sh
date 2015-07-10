ba2940b2fed5319fc9cdfc87be84f15d98d28380
Bas van Kuijck <kolnedra@gmail.com>
2015-07-10 16:58:51 +0200

    Update missing_translations.sh

--- missing_translations.sh
+++ missing_translations.sh
@@ -3,43 +3,58 @@ NSLocalizedString="NSLocalizedString"
 
 IFS_backup=$IFS
 IFS=$'\r\n\t'
-localizationFiles=($(find . -not -path "./Pods/*" -not -path "*.bundle*" -name Localizable.strings -type f))
+localizationFiles=($(find . -not -path "./Pods/*" -not -path "*.bundle*" -not -path "./.svn/*" -name Localizable.strings -type f))
 
 # Does the project contain any Localizable.strings files?
 if [ "${#localizationFiles[@]}" -ne 0 ] ; then
-foundMissingTranslation=false
-declare -a wordsDone
 
-# First search for all the NSLocalizedString() calls in the entire project (only .m files)
-lines=($(egrep -rho --include="*.m" --exclude-dir=Pods "${NSLocalizedString}\(@\".+?\"" .))
 
-for ((i=0;i<${#lines[*]};i++)); do
-word="${lines[$i]}"
+    foundMissingTranslation=false
+    declare -a wordsDone
 
-# Strip NSLocalizedString(@", so only "<word>" remains
-word=${word:((${#NSLocalizedString} + 2)):((${#word} - NSLocalizedStringLength))}
+    # First search for all the NSLocalizedString() calls in the entire project (only .m files)
+    lines=($(egrep -rho --include="*.m" --exclude-dir=Pods "${NSLocalizedString}\(@\".+?\"" .))
 
-# Iterate through the localization files
-for ((a=0;a<${#localizationFiles[*]};a++)); do
-file="${localizationFiles[$a]}"
-wordFile="[${file}:${word}]"
-# If <word> isn't checked yet in <file>
-if [[ "${wordsDone[*]}" != *"$wordFile"* ]]; then
-total="$(cat $file | grep -c "^$word")"
+    for ((i=0;i<${#lines[*]};i++)); do
+        word="${lines[$i]}"
 
-# Find the total occurences of <word> at the beginning of the line in <file>
-if [ "$total" == "0" ] ; then
-echo "$file:0: error: Missing translation for $word"
-foundMissingTranslation=true
+        # Strip NSLocalizedString(@", so only "<word>" remains
+        word=${word:((${#NSLocalizedString} + 2)):((${#word} - NSLocalizedStringLength))}
+
+        # Iterate through the localization files
+        for ((a=0;a<${#localizationFiles[*]};a++)); do
+            file="${localizationFiles[$a]}"
+            wordFile="[${file}:${word}]"
+            # If <word> isn't checked yet in <file>
+            if [[ "${wordsDone[*]}" != *"$wordFile"* ]]; then
+                total="$(cat $file | grep -c "^$word")"
+
+                # Find the total occurences of <word> at the beginning of the line in <file>
+                if [ "$total" == "0" ] ; then
+                    echo "$file:0: error: Missing translation for $word"
+                    foundMissingTranslation=true
+                fi
+                wordsDone[${#wordsDone[@]}]="$wordFile"
+            fi
+        done
+    done
+
+    # Check for duplicate keys
+    for ((a=0;a<${#localizationFiles[*]};a++)); do
+        FILENAME="${localizationFiles[$a]}"
+
+        DUPES=`cut -d' ' -f1 "$FILENAME" | sort | uniq -d`
+
+        while read -r line; do
+            if [[ $line == "\""* ]] ;
+            then
+                echo "$file:0: warning: $line used multiple times"
+            fi
+        done <<< "$DUPES"
+    done
 fi
-wordsDone[${#wordsDone[@]}]="$wordFile"
-fi
-done
-done
-fi
-
 IFS=$IFS_backup
 
 if $foundMissingTranslation; then
-exit 1
+    exit 1
 fi
